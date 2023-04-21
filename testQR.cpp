714d413a202ecd1b838044551a03334b0e671f0d
Melih Altun <40482921+melihaltun@users.noreply.github.com>
2023-04-20 19:10:58 -0700

    Added a second test case and file header to testQR.cpp

--- testQR.cpp
+++ testQR.cpp
@@ -1,3 +1,8 @@
+/**
+* @file testQR.cpp
+* @author Melih Altun @2023
+**/
+
 #include "QR.h"
 int main() {
     float A[] = { 8, 9, 3, 10,
@@ -6,12 +11,23 @@ int main() {
 
     float Q[3 * 3], R[3 * 4], B[3 * 4];
 
+
     QR(Q, R, A, 3, 4);
 
     multiply_matrices(B, Q, R, 3, 3, 4);  // B should be identical to A
 
-	return 0;
+    float AA[4 * 3];
 
+    float QQ[4 * 4], RR[3 * 3], BB[4*3];
+
+    transpose(AA, A, 3, 4);
+
+    QR(QQ, RR, AA, 4, 3);
+
+    multiply_matrices(BB, QQ, RR, 4, 4, 3);  // BB should be identical to AA
+
+	return 0;
+}
 
 /*
 // from matlab //
@@ -37,4 +53,28 @@ R =
          0         0    9.5321    8.9891
 
 */
-}
+
+/*
+    AA =
+
+        8     9     1
+        9     6     1
+        3     5    10
+        10    2    10
+
+>> [QQ, RR] = qr(AA)
+
+        QQ =
+
+        -0.5020    0.5927    0.2698    0.5692
+        -0.5647    0.0445    0.3867   -0.7277
+        -0.1882    0.4674   -0.8229   -0.2626
+        -0.6275   -0.6544   -0.3170    0.2784
+
+
+        RR =
+
+        -15.9374   -10.1020   -9.2236
+               0     6.6294   -1.2335
+               0          0   -10.7426
+*/
